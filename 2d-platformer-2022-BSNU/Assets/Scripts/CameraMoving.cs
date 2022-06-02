f3d27225127afda94ba60e280a4195547c7b05c4
JustJimer <46703655+JustJimer@users.noreply.github.com>
2022-06-02 22:32:11 +0300

    Graphics and Camera
    Level design. Parallax background. Camera tracking

---
+++ 2d-platformer-2022-BSNU/Assets/Scripts/CameraMoving.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraMoving : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }
+}
