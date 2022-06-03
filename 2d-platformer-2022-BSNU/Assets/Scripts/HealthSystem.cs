4266041e355292a1d6ccdc579c2f027548fb528d
JustJimer <46703655+JustJimer@users.noreply.github.com>
2022-06-03 17:55:41 +0300

    Invulnerability

--- 2d-platformer-2022-BSNU/Assets/Scripts/HealthSystem.cs
+++ 2d-platformer-2022-BSNU/Assets/Scripts/HealthSystem.cs
@@ -1,7 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class HealthSystem : MonoBehaviour
 {
+    // health system
+    [Header("Health")]
     [SerializeField] private float respawnHealth;
 
     public float HealthPoints { get; private set; }
@@ -9,11 +12,19 @@ public class HealthSystem : MonoBehaviour
     private Animator anim;
     private bool isDead;
 
+    // invulnerability system
+    [Header("Invunerability")]
+    [SerializeField] private float invunerabilityDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRenderer;
+
     void Start()
     {
         HealthPoints = respawnHealth;
         anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
     public void TakeDamage(float damage)
     {
         HealthPoints = Mathf.Clamp(HealthPoints - damage, 0, respawnHealth);
@@ -21,7 +32,7 @@ public class HealthSystem : MonoBehaviour
         if (HealthPoints > 0)
         {
             anim.SetTrigger("damage");
-            //iframes
+            StartCoroutine(Invunerability());
         }
         else
         {
@@ -33,10 +44,22 @@ public class HealthSystem : MonoBehaviour
             }
         }
     }
+
     public void AddHealth(float value)
     {
         HealthPoints = Mathf.Clamp(HealthPoints + value, 0, respawnHealth);
     }
 
-
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRenderer.color = new Color(1, 0, 0, 0.75f);
+            yield return new WaitForSeconds(invunerabilityDuration / (numberOfFlashes * 2));
+            spriteRenderer.color = Color.white;
+            yield return new WaitForSeconds(invunerabilityDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+    }
 }
